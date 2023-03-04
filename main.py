6797fd9e6e97abde602fb87e6c61fcf3b9a43063
stas <stas37951@gmail.com>
2023-03-04 08:37:59 +0200

    script done

--- main.py
+++ main.py
@@ -1,37 +1,74 @@
-from googletrans import Translator
-from bs4 import BeautifulSoup
+import json
 import os
+import time
+import httpcore
+import googletrans
+import glob
+from bs4 import BeautifulSoup
+from googletrans import Translator
+
+translator = Translator(service_urls=['translate.google.com'])
 
 
-translator = Translator()
+def is_translatable(text):
+    for char in text:
+        if ord(char) > 127:
+            return False
+    return True
 
 
-source_lang = input("Введіть мову, з якої потрібно перекласти текст: ")
+def translate_text(text, max_attempts=3):
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            translated_text = translator.translate(text, dest='hi').text
+            return translated_text
+        except httpcore._exceptions.ReadTimeout:
+            print("ReadTimeout exception occurred. Retrying after 3 seconds...")
+            time.sleep(3)
+            attempts += 1
+        except json.decoder.JSONDecodeError:
+            print("JSONDecodeError exception occurred. Retrying after 3 seconds...")
+            time.sleep(3)
+            attempts += 1
+        except AttributeError:
+            print("AttributeError exception occurred. Retrying after 3 seconds...")
+            time.sleep(3)
+            attempts += 1
+        except Exception as e:
+            print(f"Unknown exception occurred: {e}. Retrying after 3 seconds...")
+            time.sleep(3)
+            attempts += 1
+    print(f"Failed to translate text after {max_attempts} attempts.")
+    return None
 
 
-target_lang = input("Введіть мову, на яку потрібно перекласти текст: ")
+def translate_html_file(file_path, dest='hi'):
+    with open(file_path, 'r+', encoding='utf-8') as f:
+        soup = BeautifulSoup(f.read(), 'html.parser')
+        for tag in soup.find_all(string=True):
+            if not tag or not tag.strip() or tag.parent.name in ['script', 'style'] or tag.endswith('.com') or tag is None:
+                continue
+            if is_translatable(tag):
+                tag_text = tag.strip()
+                if not tag_text.startswith(' '):
+                    translated_text = translate_text(tag_text)
+                    if translated_text is not None:
+                        translated_text = translated_text or ''
+                        print(f"{tag} -> {translated_text}")
+                        tag.replace_with(translated_text)
+    with open(file_path, 'w+', encoding='utf-8') as f:
+        f.write(str(soup))
 
 
-html_folder_path = input("Введіть шлях до теки з HTML файлами: ")
+def translate_html_files_in_directory(directory_path):
+    html_files = glob.glob(directory_path + '/**/*.html', recursive=True)
+    if not html_files:
+        print("No HTML files found in the specified directory.")
+        return
+    for file_path in html_files:
+        translate_html_file(file_path)
 
 
-for filename in os.listdir(html_folder_path):
-    if filename.endswith(".html"):
-        file_path = os.path.join(html_folder_path, filename)
-
-        with open(file_path, "r") as f:
-            html = f.read()
-
-        soup = BeautifulSoup(html, "html.parser")
-
-        for tag in soup.find_all(text=True):
-            if tag.parent.name not in ['script', 'style']:
-                translation = translator.translate(tag, src=source_lang, dest=target_lang)
-                tag.replace_with(translation.text)
-
-        with open(file_path, "w") as f:
-            f.write(str(soup))
-
-
-
+translate_html_files_in_directory('.')
 
